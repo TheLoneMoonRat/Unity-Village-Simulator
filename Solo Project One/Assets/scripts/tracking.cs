8bc5d923a911c21cd29e933696d78a166add52a9
Nathan <nhnwong@gmail.com>
2022-12-25 14:20:02 -0500

    little bit of an improvement on tracking

--- Solo Project One/Assets/scripts/tracking.cs
+++ Solo Project One/Assets/scripts/tracking.cs
@@ -1,30 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class tracking : MonoBehaviour
 {
     public Transform treegroup;
     Vector3 currentDistance;
-    currentDistance.x = treegroup.GetChild(0).transform.position.x;
-    currentDistance.z = treegroup.GetChild(0).transform.position.z;
-    int closest = Math.Sqrt(Math.Pow(currentDistance.x, 2) + Math.Pow(currentDistance.z, 2)) < closest;
-    public gameObject target;
+    
+    float closest;
+    public Transform target;
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentDistance = new Vector3(treegroup.GetChild(0).transform.position.x, 0, treegroup.GetChild(0).transform.position.z);
+        closest = Math.Sqrt(Math.Pow(currentDistance.x, 2.0f) + Math.Pow(currentDistance.z, 2.0f));
     }
 
     // Update is called once per frame
     void Update()
     {
         for (int i=0; i< treegroup.childCount; i++) {
-            currentDistance.x = treegroup.GetChild(i).transform.position.x;
-            currentDistance.z = treegroup.GetChild(i).transform.position.z;
-            if (Math.Sqrt(Math.Pow(currentDistance.x, 2) + Math.Pow(currentDistance.z, 2)) < closest) {
+            currentDistance = new Vector3(treegroup.GetChild(0).transform.position.x, 0, treegroup.GetChild(0).transform.position.z);
+            if (Math.Sqrt(Math.Pow(ToSingle(currentDistance.x), 2) + Math.Pow(ToSingle(currentDistance.z), 2)) < closest) {
                 target = treegroup.GetChild(i);
-                closest = Math.Sqrt(Math.Pow(currentDistance.x, 2) + Math.Pow(currentDistance.z, 2));
+                closest = Math.Sqrt(Math.Pow(currentDistance.x, 2f) + Math.Pow(currentDistance.z, 2f));
             } 
         } 
 
